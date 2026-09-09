40d49d9dbfaabff78bf7b1d778689e2c606c419d
Sthembiso Mfusi <sthembiso.mfusi@bbd.co.za>
2026-09-09 14:31:07 +0200

    Feat: Added and finalized testing for title routes

--- tests/title.test.ts
+++ tests/title.test.ts
@@ -11,11 +11,35 @@ describe('Title routes', ()=> {
     it('1. Should be able to search for a title using its name', async () => {
         const response = await app.inject({
             method: 'GET',
-            url: '/title/search?q=The+Dark+Knight',
+            url: '/titles/search?q=superman',
         })
         const body = response.json();
-        expect(response.statusCode, `Expected to get a 200 status code but got this ${response.statusCode}`).toBe(200);
-        expect(body.sucess, `Expected it to be true`).toBe(true);
+        expect(response.statusCode, `Expected to get a status code 200 but got this ${response.statusCode}`).toBe(200);
+        expect(body.success, `Expected it to be true`).toBe(true);
     })
+    it('2. Should be able to find a title using its id', async () =>{
+        const response = await app.inject({
+            method: 'GET',
+            url: '/titles/tt1056948'
+        })
+        const body = response.json();
+        expect(response.statusCode, `Expected to get a status code 200 but got this ${response.statusCode}`)
+        expect(body.title, `Expected to return the requested title`).toBeDefined();
+    })
+    it('3. Should be able to find the top rated titles', async () => {
+        const response = await app.inject({
+            method: 'GET',
+            url: '/titles/top-rated'
+        })
+        const body = response.json();
+        const titles = body.topRatedTitles;
+        const ratings = titles.map(title => ({
+            id: title.tconst,
+            rating: title.Rating.averageRating
+        }))
+        ratings.filter(rate => rate.rating >= 8);
+        expect(ratings.length === titles.length, `Expected all items returned to be titles with ratings above 8`)
+        
+    }) 
 
 })
